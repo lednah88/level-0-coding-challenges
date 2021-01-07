e6b22be271489db177885da638351b1080d9ba94
lednah88 <lednah97@gmail.com>
2021-01-07 13:58:57 +0200

    task9 made general

--- task9.js
+++ task9.js
@@ -1,10 +1,10 @@
-function sumOfMultiples() {
+function sumOfMultiples(n) {
     let sum = 0;
-    for (let i = 0; i < 1000; i++) {
+    for (let i = 0; i < n; i++) {
       if (i % 3 == 0 || i % 5 == 0) {
         sum += i;
       }
     }
     return sum;
   }
-  console.log(sumOfMultiples());
+  console.log(sumOfMultiples(1000));
